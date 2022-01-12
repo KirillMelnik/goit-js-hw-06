2a10fca6751df06b321bcb248de4edfc0cfbb81e
Kirill Melnik <k.melnik@letyshops.com>
2022-01-12 23:13:22 +0200

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  if (
+      evt.currentTarget.elements.email.value === '' ||
+      evt.currentTarget.elements.password.value === ''
+  )
+  {
+    alert('All form fields must be filled out!');
+  }
+    
+  else {
+      const formData = {
+          email: evt.currentTarget.elements.email.value,
+          password: evt.currentTarget.elements.password.value,
+      }
+      console.log(formData)
+      evt.currentTarget.reset();
+    }   
+}
