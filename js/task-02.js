5102e887be4c8c22e0aef8ef063efcddd6f0b99d
Kirill Melnik <k.melnik@letyshops.com>
2022-01-12 19:18:39 +0200

    Task 3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listElem = document.querySelector('#ingredients');
+
+const createMarkup = [];
+
+ingredients.map(item => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = item;
+  itemEl.classList.add('item');
+  createMarkup.push(itemEl);
+});
+
+listElem.append(...createMarkup);
