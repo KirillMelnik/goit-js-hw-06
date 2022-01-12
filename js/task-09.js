2de9d0c2b86d56ce2dace6dd45f97bd0bb8e4930
Kirill Melnik <k.melnik@letyshops.com>
2022-01-12 23:53:44 +0200

    Task 9-10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const colorName = document.querySelector('p .color');
+const btn = document.querySelector('.change-color');
+
+btn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  colorName.textContent = randomColor;
+})
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const refs = {
+  controls: document.querySelector('#controls'),
+  input: document.querySelector('input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  boxesEl: document.querySelector('#boxes'),
+}
+
+let initialBoxSize = 30;
+const boxSizeStep = 10;
+
+function createBoxes(amount) {
+  for (let i = 1; i <= amount; i++) {
+    const box = `<div style="width: ${initialBoxSize}px; height: ${initialBoxSize}px; background-color: ${getRandomHexColor()};"></div>`
+    initialBoxSize += boxSizeStep;
+    refs.boxesEl.insertAdjacentHTML('beforeend', box);
+  }
+}
+
+function destroyBox() {
+  refs.boxesEl.innerHTML = '';
+}
+
+refs.createBtn.addEventListener('click', () => {
+  createBoxes(refs.input.value);
+});
+
+refs.destroyBtn.addEventListener('click', () => {
+  destroyBox();
+  refs.input.value = '';
+})
